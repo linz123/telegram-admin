12c11f1da1aba116cff9a2aaa67d3ccb1779335f
linz <linz@gmail.com>
2021-10-26 16:44:12 +0700

    广告推送

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const url = (window.location.href.indexOf('192') || window.location.href.indexOf('localhost'))
+const url = (window.location.href.indexOf('192') > -1 || window.location.href.indexOf('localhost') > -1)
     ? '192.168.20.254' : '45.77.249.168';
 
 const Config = {
